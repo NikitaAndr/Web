2c5f23823f4cbf91a35f75855efa32c4525f93f6
Nikita <Entvk20071@mail.ru>
2023-03-26 16:04:49 +0300

    Изображение Марса

--- main.py
+++ main.py
@@ -15,7 +15,12 @@ def index():
 
 @app.route('/promotion')
 def promotion():
-    return render_template('base.html')
+    return render_template('promotion.html')
+
+
+@app.route('/image_mars')
+def image_mars():
+    return render_template('image_mars.html')
 
 
 if __name__ == '__main__':

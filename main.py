43ab46ff02ea9b4abc8a467a72b0f58dbde893eb
Nikita <Entvk20071@mail.ru>
2023-03-27 22:09:49 +0300

    Пейзажи Марса спросить

--- main.py
+++ main.py
@@ -34,5 +34,10 @@ def astronaut_selection():
         return render_template('astronaut_selection.html')
 
 
+@app.route('/carousel')
+def carousel():
+    return render_template('carousel.html')
+
+
 if __name__ == '__main__':
     app.run(port=8080, host='127.0.0.1')
